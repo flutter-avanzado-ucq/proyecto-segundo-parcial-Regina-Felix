8398f6dedeb69467db175e29ca81f86914911dda
Regina-Felix <lgarcia32381@ucq.edu.mx>
2025-07-01 18:49:44 -0600

    Practica 24 junio correcta

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_animaciones_notificaciones/main.dart';
+import 'package:tareas/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
